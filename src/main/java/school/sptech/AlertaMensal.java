f8ae622810f1e3d0dfd2e46b0b820a8fac565f17
lizzvb <liz.bohn@sptech.school>
2025-05-07 15:28:15 -0300

    Adicionando CRON Mensal

---
+++ src/main/java/school/sptech/AlertaMensal.java
@@ -0,0 +1,20 @@
+package school.sptech;
+
+
+
+import java.util.List;
+
+public class AlertaMensal {
+    public static void main(String[] args) {
+        List<String> mensagensMensais = List.of(
+                "📅 Início de mês! Veja como se comportaram as evasões no mês passado.",
+                "📈 Os dados acumulados do último mês revelam padrões consistentes de evasão.",
+                "🔄 Novo mês, novos desafios — mas os dados do anterior seguem prontos para análise."
+        );
+
+        Telegram telegram = new Telegram(mensagensMensais);
+        telegram.enviarMensagemAleatoria();
+    }
+}
+
+
